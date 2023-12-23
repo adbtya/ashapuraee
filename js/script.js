331cc4d4552600f0b8dca92f035d627281ad040a
dedomil <dedomil@skiff.com>
2023-12-23 20:14:03 +0530

    add recaptcha

--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
 let form = document.getElementById("contact");
 
 form.addEventListener("submit", async (e) => {
-   e.preventDefault();
-   let name = document.getElementById("name").value;
-   let email = document.getElementById("email").value;
-   let number = document.getElementById("number").value;
-   let message = document.getElementById("message").value;
-   form.reset();
-   fetch("https://api.ashapuraee.com/", {
-      method: "POST",
-      body: JSON.stringify({
-         "name": `${name}`,
-         "email": `${email}`,
-         "contact": `${number}`,
-         "message": `${message}`,
-         "secret": "kjyhdwejqgrfgewiurfgui"
-      }),
-      headers: {
-         "Content-Type": "application/json"
-      }
-   }).then(data => data.json()).then(data => alert(data.message));
+  e.preventDefault();
+
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+  let captcha = document.getElementById("g-recaptcha-response").value;
+
+  form.reset();
+
+  fetch("https://contact-api.dedomil.workers.dev/", {
+    method: "POST",
+    body: JSON.stringify({
+      name,
+      email,
+      message,
+      captcha,
+    }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(data => data.json()).then(data => alert(data.message));
 });
